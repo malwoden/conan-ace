521638cb683a7f57154e3e395fb537841c5b2a21
Neil Williams <neewill@gmail.com>
2018-02-26 23:36:55 +0000

    clean up dead code in conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,6 @@
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
 import os
 
-
 class AceConan(ConanFile):
     name = "ace"
     version = "6.4.6"
@@ -14,15 +13,6 @@ class AceConan(ConanFile):
 
     def source(self):
         tools.get("https://github.com/DOCGroup/ACE_TAO/releases/download/ACE+TAO-%s/ACE-src.tar.gz" % self.version.replace('.', '_'))
-
-#     def source(self):
-#         self.run("git clone https://github.com/memsharded/hello.git")
-#         self.run("cd hello && git checkout static_shared")
-#         # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
-#         # if the packaged project doesn't have variables to set it properly
-#         tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)", '''PROJECT(MyHello)
-# include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-# conan_basic_setup()''')
 
     def build(self):
         ace_wrappers_path_abs = self.source_folder + "/ACE_wrappers"
@@ -49,10 +39,6 @@ class AceConan(ConanFile):
         else:
             raise tools.ConanException("Build not setup for %s" % self.settings.os)
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s' % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
         install_src_abs = self.source_folder + "/ACE_wrappers/build_install"
         self.copy("*", dst="include", src=install_src_abs + "/include")
@@ -62,6 +48,4 @@ class AceConan(ConanFile):
 
     def package_info(self):
         if self.settings.os == "Linux":
-            self.cpp_info.libs = tools.collect_libs(self)
-            # self.cpp_info.libs = ["libACE_Compression.so", "libACE_ETCL_Parser.so", "libACE_ETCL.so", "libACE_Monitor_Control", "libACE_RLECompression", "libACE.so"]
- 
+            self.cpp_info.libs = tools.collect_libs(self) 
